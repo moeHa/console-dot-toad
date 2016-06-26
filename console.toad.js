17f538d2df5249e1ee3923ec9b4e860615f2e024
Dafrok <o.o@mug.dog>
2016-06-26 13:49:37 +0800

    Excited!

---
+++ console.toad.js
@@ -0,0 +1,57 @@
+  /**
+   *  One copy of console.frog.
+   *  Console.toad. A better way to log!
+   */
+
+  (function dothething() {
+
+    if( !window.console ) {
+      return;
+    }
+
+    window.console.toad = function() {
+
+      var i,
+        css = "color: #333";
+
+      var inBrowser = (typeof window !== 'undefined');
+
+      var toad;
+      if( inBrowser ) {
+        toad = ['%c%c',
+          "%c/_____    _____\\%c",
+          "%c|  \\\\ |__|  \\\\ |%c",
+          "%c|_____/  \\_____|%c",
+          "%c     /^ ^ \\     %c",
+          "%c   (  ____; )  %c",
+          '%c%c'];
+      } else {
+        toad = ['',
+          "/_____    _____\\",
+          "|  \\\\ |__|  \\\\ |",
+          "|_____/  \\_____|",
+          "     /^ ^ \\     ",
+          "   (  ____; )  ",
+          ''];
+      }
+
+      var args = Array.prototype.slice.call(arguments);
+      var stringOfArgs = args.join(' ');
+
+      if( stringOfArgs.length > 0 ) {
+        toad[1] = toad[1] + "   ---" + ("-".repeat(stringOfArgs.length)) + "-";
+        toad[2] = toad[2] + "-(   " + stringOfArgs + "   )";
+        toad[3] = toad[3] + "   ---" + ("-".repeat(stringOfArgs.length)) + "-";
+      }
+
+      if( inBrowser ) {
+        for( i = 0; i < toad.length; i++ ) {
+          console.log(toad[i], css, "");
+        }
+      } else {
+        for( i = 0; i < toad.length; i++ ) {
+          console.log(toad[i]);
+        }
+      }
+    }
+  })();
---
+++ readme.md
@@ -0,0 +1,46 @@
+One copy from [console.frog](https://github.com/tholman/console-dot-frog).
+
+###console.toad
+
+`console.toad` is an easy way to spice up your development life. Its really that simple! Runs on vanilla javascript, in most modern browsers (the kind that has developer tools!). You can see it live [here](http://tholman.com/console-dot-toad).
+
+### Preview
+
+So, here's what regular life looks like:
+
+```javascript
+// Here is you, logging some logs
+console.log("Boring.");
+```
+```
+// And here is your output.
+Boring.
+```
+
+Wow, what a drag that was! No wonder you've become disenchanted with web development.
+
+**NOW!** Here's what your life could be after `console.toad`
+
+```javascript
+// Here is happy you, logging some logs
+console.toad("Excited!");
+```
+
+```
+// And here is your output.
+/_____    _____\   ------------
+|  \\ |__|  \\ |-(   Excited!   )
+|_____/  \_____|   ------------
+     /^ ^ \     
+   (  ____; )  
+```
+
+Wow. That feels good!
+
+###Usage
+Simply include the `console.toad.js` file into your app!
+
+###License
+Copyright (c) 2016 Dafrok Zhang - http://dafrok.github.io
+
+The MIT License
